2700077e023b9ffb0d0dfd0e1737189f0122615e
Aloysius Teo <aloysiusteo72@gmail.com>
2023-08-02 23:01:28 +0000

    Update 4 files

--- app/components/hero.js
+++ app/components/hero.js
@@ -6,7 +6,7 @@ export default function Hero() {
     <div className="hero min-h-screen">
       <div className="hero-content text-center">
         <div className="max-w-md">
-          <h1 className="animate-linear bg-gradient-to-r from-fuchsia-500 via-zinc-900 to-blue-500 bg-[length:200%_auto] bg-clip-text text-center text-8xl font-black uppercase text-transparent xl:text-9xl">
+          <h1 className="animate-linear bg-gradient-to-r from-fuchsia-500 via-amber-500 to-blue-500 bg-[length:200%_auto] bg-clip-text text-center text-8xl font-black uppercase text-transparent xl:text-9xl">
             4 Mo Beers!
           </h1>
           <Player
